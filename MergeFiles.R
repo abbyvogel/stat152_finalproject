68f2ca1ef9f3ccb32447031b82d11271231c56a6
Cindy Kang <cindykang@berkeley.edu>
2017-04-27 12:44:04 -0700

    code for full_data_v3

--- MergeFiles.R
+++ MergeFiles.R
@@ -10,11 +10,13 @@ library(dplyr)
 list_dfs <- list(demographics, bmi, activity, weights)
 
 full_data <- join_all(list_dfs) %>% 
-  select(seqn, riagendr, ridageyr, bmdbmic, whq030m, whq500, paq744, paq746, paq750) %>% 
+  select(seqn, wtint2yr, wtmec2yr, sdmvpsu, sdmvstra, riagendr, ridageyr, 
+         bmdbmic, whq030m, whq500, paq744, paq746, paq750) %>% 
   filter(ridageyr %in% c(12:15)) %>% 
-  dplyr::rename(`id` = seqn, `gender` = riagendr, `age` = ridageyr, `bmi` = bmdbmic, 
-                `opinion_wt` = whq030m, `action_wt` = whq500, pe_yn = paq744, 
-                freq_pe = paq746, enjoy_pe = paq750) %>% 
+  dplyr::rename(`id` = seqn, `int_wt` = wtint2yr, `exam_wt` = wtmec2yr, `psu` = sdmvpsu, 
+                `stratum` = sdmvstra, `gender` = riagendr, `age` = ridageyr, `bmi` = bmdbmic, 
+                `opinion_wt` = whq030m, `action_wt` = whq500, `pe_yn` = paq744, 
+                `freq_pe` = paq746, `enjoy_pe` = paq750) %>% 
   mutate(gender = factor(gender, levels = 1:2, labels = c("M", "F")),
          bmi = factor(bmi, levels = 1:4, 
                       labels = c("underweight", "normal", "overweight", "obese")),
@@ -29,13 +31,9 @@ full_data <- join_all(list_dfs) %>%
                            labels = c("strongly agree", "agree", "neither agree nor disagree", 
                                       "disagree", "strongly disagree", "refused", "unsure"))) %>% 
   mutate(freq_pe = ifelse(pe_yn == "no", 0, freq_pe))
-  
 
-#Fix missing values, throw out units with more than four missing responses.
-full_data$bmi[c(552, 654, 421, 422)] <- "normal"
-full_data = full_data[complete.cases(full_data), ]
 
-write.csv(full_data, "~/Desktop/Stat152/FinalProject/full_data_v2.csv")
+write.csv(full_data, "~/Desktop/Stat152/FinalProject/full_data_v3.csv")
 
 
 
