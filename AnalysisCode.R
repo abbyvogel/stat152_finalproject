2594ea5e4c4e28ade9f3923e6c9622914491d31c
Cindy Kang <cindykang@berkeley.edu>
2017-05-05 15:18:13 -0700

    changed weights to exam weights

--- AnalysisCode.R
+++ AnalysisCode.R
@@ -3,7 +3,7 @@ library(survey)
 
 nhanes = read.csv("~/Desktop/Stat152/FinalProject/full_data_v4.csv")[ , -c(1,2)] 
 nhanes_design = svydesign(ids = ~psu+id, strata = ~stratum, 
-                          nest = TRUE, weights = ~int_wt, data = nhanes)
+                          nest = TRUE, weights = ~exam_wt, data = nhanes)
 
 
 #Proportion in Each Weight Category by Frequency of PE
@@ -11,5 +11,7 @@ svymean(~interaction(freq_pe, bmi), design = nhanes_design)
 
 #Perceived and Actual Weight Category by Frequency of PE
 svymean(~interaction(freq_pe, bmi, opinion_wt), design = nhanes_design)
+
+
 
 
