7913af18e63408a484c1935cfaf01802f5be1050
Cindy Kang <cindykang@berkeley.edu>
2017-05-05 23:40:08 -0700

    added code for barplots

--- AnalysisCode.R
+++ AnalysisCode.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(survey)
+library(ggplot2)
 
 nhanes = read.csv("~/Desktop/Stat152/FinalProject/full_data_v4.csv")[ , -c(1,2)] %>% 
   mutate(bmi1 = as.factor(ifelse(bmi == "obese", "overweight", as.character(bmi)))) %>% 
@@ -15,12 +16,21 @@ bmi_prop_tbl = round(prop.table(bmi_tbl), 5)
 summary(bmi_tbl, statistic = "F")
 svychisq(~freq_pe+bmi, nhanes_design, statistic = "F")
 
-#Proportion in Each Weight Category by Existence of PE
-svymean(~interaction(pe_yn, bmi), design = nhanes_design)
-bmi_tbl = svytable(~pe_yn+bmi, nhanes_design)
-bmi_prop_tbl = round(prop.table(bmi_tbl), 5)
-summary(bmi_tbl, statistic = "F")
-svychisq(~pe_yn+bmi, nhanes_design, statistic = "F")
+#Barplot
+bmi_prop_df = as.data.frame(bmi_prop_tbl)
+prop_freq_pe = tapply(bmi_prop_df$Freq, bmi_prop_df$freq_pe, sum)
+bmi_prop_df$prop_freq_pe = prop_freq_pe[bmi_prop_df$freq_pe]
+plot_df = bmi_prop_df %>% 
+  mutate(std_prop = Freq/prop_freq_pe)
+
+ggplot(plot_df, aes(freq_pe, std_prop)) +   
+  geom_bar(aes(fill = bmi), position = "dodge", stat="identity") + 
+  labs(x = "Frequency of PE",
+       y = "Proportion in Each Weight Category",
+       fill = "Weight Category",
+       title = "Proportion of Students in Each Weight Category by Frequency of PE")
+
+######################################################################################
 
 #Perceived and Actual Weight Category by Frequency of PE
 svymean(~interaction(freq_pe, bmi, opinion_wt), design = nhanes_design)
@@ -29,12 +39,21 @@ perception_prop_tbl = round(prop.table(perception_tbl), 5)
 summary(perception_tbl, statistic = "F")
 svychisq(~freq_pe+correct_perception, nhanes_design, statistic = "F")
 
-#Perceived and Actual Weight Category by Existence of PE
-svymean(~interaction(pe_yn, bmi, opinion_wt), design = nhanes_design)
-perception_tbl = svytable(~pe_yn+correct_perception, nhanes_design)
-perception_prop_tbl = round(prop.table(perception_tbl), 5)
-summary(perception_tbl, statistic = "F")
-svychisq(~pe_yn+correct_perception, nhanes_design, statistic = "F")
+#Barplot
+perception_prop_df = as.data.frame(perception_prop_tbl)
+prop_freq_pe = tapply(perception_prop_df$Freq, perception_prop_df$freq_pe, sum)
+perception_prop_df$prop_freq_pe = prop_freq_pe[perception_prop_df$freq_pe]
+plot_df = perception_prop_df %>% 
+  mutate(std_prop = Freq/prop_freq_pe)
+
+ggplot(plot_df, aes(freq_pe, std_prop)) +   
+  geom_bar(aes(fill = correct_perception), position = "dodge", stat="identity") + 
+  labs(x = "Frequency of PE",
+       y = "Proportion Whose Weight Perception Matched BMI Category",
+       fill = "Correct Perception",
+       title = "Proportion of Students Whose Weight Perception Matched BMI Category\nby Frequency of PE")
+
+######################################################################################
 
 #Proportion in Each Weight Category by Enjoyment of PE
 svymean(~interaction(enjoy_pe, bmi), design = nhanes_design)
@@ -43,9 +62,39 @@ bmi_prop_tbl = round(prop.table(bmi_tbl), 5)
 summary(bmi_tbl, statistic = "F")
 svychisq(~enjoy_pe+bmi, nhanes_design, statistic = "F")
 
+#Barplot
+bmi_prop_df = as.data.frame(bmi_prop_tbl)
+prop_enjoy_pe = tapply(bmi_prop_df$Freq, bmi_prop_df$enjoy_pe, sum)
+bmi_prop_df$prop_enjoy_pe = prop_enjoy_pe[bmi_prop_df$enjoy_pe]
+plot_df = bmi_prop_df %>% 
+  mutate(std_prop = Freq/prop_enjoy_pe)
+
+ggplot(plot_df, aes(enjoy_pe, std_prop)) +   
+  geom_bar(aes(fill = bmi), position = "dodge", stat="identity") + 
+  labs(x = "Enjoyment of PE",
+       y = "Proportion in Each Weight Category",
+       fill = "Weight Category",
+       title = "Proportion of Students in Each Weight Category by Enjoyment of PE")
+
+######################################################################################
+
 #Perceived and Actual Weight Category by Enjoyment of PE
 svymean(~interaction(enjoy_pe, bmi, opinion_wt), design = nhanes_design)
 perception_tbl = svytable(~enjoy_pe+correct_perception, nhanes_design)
 perception_prop_tbl = round(prop.table(perception_tbl), 5)
 summary(perception_tbl, statistic = "F")
 svychisq(~enjoy_pe+correct_perception, nhanes_design, statistic = "F")
+
+#Barplot
+perception_prop_df = as.data.frame(perception_prop_tbl)
+prop_enjoy_pe = tapply(perception_prop_df$Freq, perception_prop_df$enjoy_pe, sum)
+perception_prop_df$prop_enjoy_pe = prop_enjoy_pe[perception_prop_df$enjoy_pe]
+plot_df = perception_prop_df %>% 
+  mutate(std_prop = Freq/prop_enjoy_pe)
+
+ggplot(plot_df, aes(enjoy_pe, std_prop)) +   
+  geom_bar(aes(fill = correct_perception), position = "dodge", stat="identity") + 
+  labs(x = "Enjoyment of PE",
+       y = "Proportion Whose Weight Perception Matched BMI Category",
+       fill = "Correct Perception",
+       title = "Proportion of Students Whose Weight Perception Matched BMI Category\nby Enjoyment of PE")
