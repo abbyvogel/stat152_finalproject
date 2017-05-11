b20ab15e3d8f3671b938741bec610f93cf8a99c2
Cindy Kang <cindykang@berkeley.edu>
2017-05-10 23:48:09 -0700

    added t test analyses and new plots

--- AnalysisCode.R
+++ AnalysisCode.R
@@ -9,6 +9,54 @@ nhanes_design = svydesign(ids = ~psu+id, strata = ~stratum,
                           nest = TRUE, weights = ~exam_wt, data = nhanes)
 
 
+#Proportion in Each Weight Category by Existence of PE
+svymean(~interaction(pe_yn, bmi), design = nhanes_design)
+bmi_tbl = svytable(~pe_yn+bmi, nhanes_design)
+bmi_prop_tbl = round(prop.table(bmi_tbl), 5)
+summary(bmi_tbl, statistic = "F")
+svychisq(~freq_pe+bmi, nhanes_design, statistic = "F")
+
+#Barplot
+bmi_prop_df = as.data.frame(bmi_prop_tbl)
+prop_pe_yn = tapply(bmi_prop_df$Freq, bmi_prop_df$pe_yn, sum)
+bmi_prop_df$prop_pe_yn = prop_pe_yn[bmi_prop_df$pe_yn]
+plot_df = bmi_prop_df %>% 
+  mutate(std_prop = Freq/prop_pe_yn)
+
+ggplot(plot_df, aes(pe_yn, std_prop)) +   
+  geom_bar(aes(fill = bmi), position = "dodge", stat="identity") + 
+  labs(x = "Existence of PE",
+       y = "Proportion in Each Weight Category",
+       fill = "Weight Category",
+       title = "Proportion of Adolescents in Each Weight Category by Existence of PE") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+######################################################################################
+
+#Proportion of Correct Perceptions by Existence of PE
+svymean(~interaction(pe_yn, correct_perception), design = nhanes_design)
+bmi_tbl = svytable(~pe_yn+correct_perception, nhanes_design)
+bmi_prop_tbl = round(prop.table(bmi_tbl), 5)
+summary(bmi_tbl, statistic = "F")
+svychisq(~pe_yn+correct_perception, nhanes_design, statistic = "F")
+
+#Barplot
+bmi_prop_df = as.data.frame(bmi_prop_tbl)
+prop_pe_yn = tapply(bmi_prop_df$Freq, bmi_prop_df$pe_yn, sum)
+bmi_prop_df$prop_pe_yn = prop_pe_yn[bmi_prop_df$pe_yn]
+plot_df = bmi_prop_df %>% 
+  mutate(std_prop = Freq/prop_pe_yn)
+
+ggplot(plot_df, aes(pe_yn, std_prop)) +   
+  geom_bar(aes(fill = correct_perception), position = "dodge", stat="identity") + 
+  labs(x = "Existence of PE",
+       y = "Proportion",
+       fill = "Correct Perception",
+       title = "Proportion of Correct Perceptions by Existence of PE") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+######################################################################################
+
 #Proportion in Each Weight Category by Frequency of PE
 svymean(~interaction(freq_pe, bmi), design = nhanes_design)
 bmi_tbl = svytable(~freq_pe+bmi, nhanes_design)
@@ -17,7 +65,7 @@ summary(bmi_tbl, statistic = "F")
 svychisq(~freq_pe+bmi, nhanes_design, statistic = "F")
 
 #Barplot
-bmi_prop_df = as.data.frame(bmi_prop_tbl)
+bmi_prop_df = as.data.frame(bmi_prop_tbl) 
 prop_freq_pe = tapply(bmi_prop_df$Freq, bmi_prop_df$freq_pe, sum)
 bmi_prop_df$prop_freq_pe = prop_freq_pe[bmi_prop_df$freq_pe]
 plot_df = bmi_prop_df %>% 
@@ -28,7 +76,8 @@ ggplot(plot_df, aes(freq_pe, std_prop)) +
   labs(x = "Frequency of PE",
        y = "Proportion in Each Weight Category",
        fill = "Weight Category",
-       title = "Proportion of Students in Each Weight Category by Frequency of PE")
+       title = "Proportion of Adolescents in Each Weight Category by Frequency of PE") +
+  theme(plot.title = element_text(hjust = 0.5))
 
 ######################################################################################
 
@@ -49,9 +98,10 @@ plot_df = perception_prop_df %>%
 ggplot(plot_df, aes(freq_pe, std_prop)) +   
   geom_bar(aes(fill = correct_perception), position = "dodge", stat="identity") + 
   labs(x = "Frequency of PE",
-       y = "Proportion Whose Weight Perception Matched BMI Category",
+       y = "Proportion",
        fill = "Correct Perception",
-       title = "Proportion of Students Whose Weight Perception Matched BMI Category\nby Frequency of PE")
+       title = "Proportion of Adolescents Whose Weight Perception Matched BMI Category\nby Frequency of PE") + 
+  theme(plot.title = element_text(hjust = 0.5))
 
 ######################################################################################
 
@@ -74,7 +124,41 @@ ggplot(plot_df, aes(enjoy_pe, std_prop)) +
   labs(x = "Enjoyment of PE",
        y = "Proportion in Each Weight Category",
        fill = "Weight Category",
-       title = "Proportion of Students in Each Weight Category by Enjoyment of PE")
+       title = "Proportion of Adolescents in Each Weight Category by Enjoyment of PE") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+######################################################################################
+
+
+#Proportion Who Enjoy PE by Weight Class
+svymean(~interaction(enjoy_pe, bmi), design = nhanes_design)
+bmi_tbl = svytable(~enjoy_pe+bmi, nhanes_design)
+bmi_prop_tbl = round(prop.table(bmi_tbl), 5)
+summary(bmi_tbl, statistic = "F")
+svychisq(~enjoy_pe+bmi, nhanes_design, statistic = "F")
+
+#Barplot
+bmi_prop_df = as.data.frame(bmi_prop_tbl) %>% 
+  mutate(enjoy_pe = factor(enjoy_pe, levels = c("strongly agree", 
+                                                "agree",
+                                                "neither agree nor disagree",
+                                                "disagree",
+                                                "strongly disagree",
+                                                "unsure"))) 
+prop_weight = tapply(bmi_prop_df$Freq, bmi_prop_df$bmi, sum)
+bmi_prop_df$prop_weight = prop_weight[bmi_prop_df$bmi]
+new_prop = bmi_prop_df$Freq/bmi_prop_df$prop_weight
+plot_df = bmi_prop_df %>% 
+  mutate(std_prop = new_prop)
+
+ggplot(plot_df, aes(bmi, new_prop)) +   
+  geom_bar(aes(fill = enjoy_pe), position = "dodge", stat="identity") + 
+  labs(x = "Weight Category by BMI ",
+       y = "Proportion",
+       fill = "Enjoyment of PE",
+       title = "Proportion of Adolescents Who Enjoy PE by Weight Category") +
+  theme(plot.title = element_text(hjust = 0.5))
+
 
 ######################################################################################
 
@@ -95,6 +179,7 @@ plot_df = perception_prop_df %>%
 ggplot(plot_df, aes(enjoy_pe, std_prop)) +   
   geom_bar(aes(fill = correct_perception), position = "dodge", stat="identity") + 
   labs(x = "Enjoyment of PE",
-       y = "Proportion Whose Weight Perception Matched BMI Category",
+       y = "Proportion",
        fill = "Correct Perception",
-       title = "Proportion of Students Whose Weight Perception Matched BMI Category\nby Enjoyment of PE")
+       title = "Proportion of Adolescents Whose Weight Perception Matched BMI Category\nby Enjoyment of PE") +
+  theme(plot.title = element_text(hjust = 0.5))
